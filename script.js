f0722bbc5863f196d4831961a703ca983628b50d
BhawishyaAccio <bhawishya@acciojob.com>
2025-05-12 03:21:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+let ans=null;
+for(let i=0;i<ans.length-1;i++)
+	{
+	    for(j=i+1;j<ans.length;j++)
+			{
+				if(str[i]!=str[j])
+				{
+					ans=str[i];
+					break;
+				}
+			}
+		if(str!=null)
+		{
+			break;
+		}
+	}
+
+return ans;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
